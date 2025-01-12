7e0b4f567dd4744ced62b02af91996058f80b08e
Limnetic <125190431+Limnetic@users.noreply.github.com>
2025-01-12 22:26:48 +0300

    1.0.5

--- DiscordRPC-Ui/General/main.py
+++ DiscordRPC-Ui/General/main.py
@@ -5,41 +5,16 @@ BASE_DIR = 'files'
 
 time_of_tick_update = 5
 
-client_id = None
-url = None
-state = None
-details = None
-start = None
-end = None
-party_size = None
-party_size_max = None
-but_text = None 
-but_link = None
+InfoList = None
 
 def read_from_fields():
-    global url, state, details, start, end, party_size, party_size_max, but_text, but_link
-    with open(f"{BASE_DIR}/BotID", "r") as f:
-        url = f.read()
-    with open(f"{BASE_DIR}/DataState", "r") as f:
-        state = f.read()
-    with open(f"{BASE_DIR}/DataDetails", "r") as f:
-        details = f.read()
-    try:
-        with open(f"{BASE_DIR}/DataStart", "r") as f:
-            start = int(f.read())
-        with open(f"{BASE_DIR}/DataEnd", "r") as f:
-            end = int(f.read())
-    except:pass
-    try:
-        with open(f"{BASE_DIR}/DataParty_size", "r") as f:
-            party_size = int(f.read())
-        with open(f"{BASE_DIR}/DataParty_sizeMax", "r") as f:
-            party_size_max = int(f.read())
-    except:pass
-    with open(f"{BASE_DIR}/ButText", "r") as f:
-        but_text = f.read()
-    with open(f"{BASE_DIR}/ButLink", "r") as f:
-        but_link = f.read()
+    global InfoList
+
+    
+    with open(f"{BASE_DIR}/InfoList.json", "r") as f:
+        InfoList = eval(f.read())
+    
+    print(InfoList)
 
 # We use URL of image of application's avatar for parse this img to UI
 # (WIP) - Will be soon
@@ -56,20 +31,24 @@ def take_client_id_from_url(url):
 def main_function():
     global client_id
     read_from_fields()
-    client_id = take_client_id_from_url(url)
+    client_id = take_client_id_from_url(InfoList['BotID'])
 
     RPC = Presence(client_id, pipe=0) 
     RPC.connect()
 
-
     while True:
         
-        buttons_pack = ([{"label": f"{but_text}", "url": f"{but_link}"}] if but_text != '' else None)
-        party_size_pack = ([party_size, party_size_max] if party_size_max != None else None)
-        state_pack = (state if state != '' else '  ')
-        details_pack = (details if details != '' else '  ')
-        start_pack = (start if start != None else None)
-        end_pack = (end if end != None else None)
+        buttons_pack = []
+        if InfoList['ButLink'] != '' and InfoList['ButText'] != '': buttons_pack.append({"label": f"{InfoList['ButText']}", "url": f"{InfoList['ButLink']}"})
+        if InfoList['ButLink2'] != '' and InfoList['ButText2'] != '': buttons_pack.append({"label": f"{InfoList['ButText2']}", "url": f"{InfoList['ButLink2']}"})
+        if buttons_pack == []: buttons_pack = None
+        
+        party_size_pack = ([int(InfoList['DataParty_size']), int(InfoList['DataParty_sizeMax'])] if InfoList['DataParty_sizeMax'] != '' else None)
+
+        state_pack = (InfoList['DataState'] if InfoList['DataState'] != '' else '  ')
+        details_pack = (InfoList['DataDetails'] if InfoList['DataDetails'] != '' else '  ')
+        start_pack = (int(InfoList['DataStart']) if InfoList['DataStart'] != '' else None)
+        end_pack = (int(InfoList['DataEnd']) if InfoList['DataEnd'] != '' else None)
         
         RPC.update(details=details_pack,
                    state=state_pack,
